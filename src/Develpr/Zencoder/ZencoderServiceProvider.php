b3c43f1d88c88ba019ae4cdd361e4e21cbed25a3
Kevin Mitchell <kevin@infielddesign.com>
2013-01-11 17:21:34 -0800

    started l4 branch over using the laravel beta workbench.

---
+++ src/Develpr/Zencoder/ZencoderServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Develpr\Zencoder;
+
+use Illuminate\Support\ServiceProvider;
+
+class ZencoderServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('develpr/zencoder');
+	}
+
+	/**
+	 * Register the {{full_package}} service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
