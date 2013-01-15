5c8361879001f2a7802b545e790e99bd923a4e67
Ben <bngreer@gmail.com>
2013-01-15 11:50:14 -0800

    Update src/Develpr/Zencoder/routes.php
    just and idea to make this configurable, but with reasonable defaults

--- src/Develpr/Zencoder/routes.php
+++ src/Develpr/Zencoder/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::any('zencoder/callback', 'Develpr\Zencoder\ZencoderController@callback');
+Route::any(Config::get('develpr/zencoder::api.handles', 'zencoder').'/callback', 'Develpr\Zencoder\ZencoderController@callback');
